11e45a41eba54f684cf5d3826b86104689c08d76
Noi-Git <noi.patsin@gmail.com>
2024-03-12 22:55:28 -0400

    add code comment

--- visits/index.js
+++ visits/index.js
@@ -8,8 +8,9 @@ client.set('visits', 0)
 
 app.get('/', (req, res) => {
   client.get('visits', (err, visits) => {
-    res.send('Number of visits is ' + visits)
-    client.set('visits', parseInt(visits + 1))
+    // get number of time the page has been visited
+    res.send('Number of visits is ' + visits) // send response back
+    client.set('visits', parseInt(visits + 1)) // update the number of time the page has been visited
   })
 })
 
